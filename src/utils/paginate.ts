69e767712574889e47832fa8b075913320a22e9b
miracleonyenma <miracleficient@gmail.com>
2025-01-17 21:17:38 +0100

    feat: enhance pagination utility with sorting options and improve query filtering

--- src/utils/paginate.ts
+++ src/utils/paginate.ts
@@ -1,4 +1,4 @@
-import { Model } from "mongoose";
+import { Model, SortOrder } from "mongoose";
 
 interface Pagination {
   page?: number;
@@ -15,22 +15,39 @@ interface PaginatedResult<T> {
   };
 }
 
+interface SortOptions {
+  by?: "createdAt" | "updatedAt";
+  direction?: "asc" | "desc";
+}
+
+const getSort = (sort?: SortOptions): { [key: string]: SortOrder } => {
+  if (!sort) return { createdAt: -1 }; // Default to sorting by `createdAt` in descending order
+  const { by = "createdAt", direction = "desc" } = sort;
+  return { [by]: direction === "asc" ? 1 : -1 };
+};
+
 const paginateCollection = async <T>(
   collection: Model<T>,
   pagination: Pagination,
   options?: {
     filter?: any;
+    sort?: SortOptions;
     populate?: string;
   }
 ): Promise<PaginatedResult<T>> => {
   const { page = 1, limit = 10 } = pagination;
   const skip = (page - 1) * limit;
+
+  const sort = getSort(options?.sort);
+
   const data = await collection
-    .find(options.filter || {})
+    .find(options?.filter || {})
     .skip(skip)
     .limit(limit)
-    .populate(options.populate || "");
-  const total = await collection.countDocuments();
+    .populate(options?.populate || "")
+    .sort(sort);
+
+  const total = await collection.countDocuments(options?.filter || {});
   const pages = Math.ceil(total / limit);
 
   return {
